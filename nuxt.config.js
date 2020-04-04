b39f8e386c6ecec608569b8cbb230dd0830df38c
yoee <yoeex@outlook.com>
2020-04-04 14:34:41 +0800

    layout CSS reset global

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ module.exports = {
  //'element-ui/lib/theme-chalk/reset.css' 是element-ui自带的
   css: [
     'element-ui/lib/theme-chalk/reset.css',
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
