74aa803838c9dca953cb7828557b01d1d9255908
yoee <yoeex@outlook.com>
2020-04-08 02:09:07 +0800

    切换城市 ==> 按拼音首字母选择

--- components/changeCity/Category.vue
+++ components/changeCity/Category.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+	<div>
 		<dl class="m-category">
 			<dt>按拼音首字母选择：</dt>
 			<dd 
 				v-for="item in list"
 			  :key="item">
-				<a href="/">
+				<a :href="'#city-' + item">
 					{{ item }}
 				</a>
 			</dd>
@@ -16,26 +16,53 @@
 		  class="m-category-section">
 			<dt :id="'city-' + item.title">{{ item.title }}</dt>
 			<dd>
-					<span>
-						{{item}}
-					</span>
+				<span v-for="city in item.cities">
+					{{ city }}
+				</span>
 			</dd>
 		</dl>
 	</div>
 </template>
 
 <script>
-  export default {
-    name: '',
-    data() {
-      return {
-				list: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-				block: ["安顺", "北京", "成都", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-      }
-    }
-  }
+import jspy from 'js-pinyin';
+export default {
+	data() {
+		return {
+			list: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+			block: []
+		}
+	},
+	async mounted() {
+		let blocks = [];
+		let { status, data: {city} } = await this.$axios.get('/geo/city');
+		if (status === 200) {
+			let firstLetter;
+			let letterPos;
+			let temp = {};
+			city.forEach(item => {
+				firstLetter = jspy.getFullChars(item.name).toLocaleLowerCase().slice(0, 1);
+				letterPos = firstLetter.charCodeAt(0);
+				if( letterPos > 96 && letterPos < 123 ) {
+					if (!temp[firstLetter]) {
+						temp[firstLetter] = [];
+					}
+					temp[firstLetter].push(item.name)
+				} 
+			})
+			for (let [key, value] of Object.entries(temp)) {
+				blocks.push({
+					title: key.toUpperCase(), 
+					cities: value            
+				})
+			}
+			blocks.sort((a, b) => a.title.charCodeAt(0) - b.title.charCodeAt(0));
+			this.block = blocks;
+		}
+	}
+}
 </script>
 
-<style lang="scss" scoped>
-	@import "@/assets/css/changeCity/category.scss";
+<style lang="scss">
+@import "@/assets/css/changeCity/category.scss";
 </style>
