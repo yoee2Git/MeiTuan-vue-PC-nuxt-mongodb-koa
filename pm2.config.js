cba425b2404341ac0536f202734afebbd250c09d
yoee <yoeex@outlook.com>
2020-05-28 16:29:45 +0800

    .

--- pm2.config.js
+++ pm2.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			repo: 'https://gitee.com/yoee/meituan-pc-nuxt.git',
 			path: '/var/www/meituan',
 			'pre-deploy': 'git fetch --all',
-			'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart pm2.config.js --env production'
+			'post-deploy': 'npm install && npm run build && npm start && pm2 pm2.config.js --env production'
 		}
 	}
 }
